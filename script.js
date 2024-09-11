69bf497fa0abbf6dd45aa23b0eb71fc224f852f5
Astrocricket <105763633+Astrocricket777@users.noreply.github.com>
2024-09-11 12:13:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,21 +10,29 @@ var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}
     attribution: '©OpenStreetMap, ©CartoDB'
 }).addTo(map);
 
-var bounds = [[0,0], [1000, 1500]];
-var image = L.imageOverlay('Image2.png', bounds).addTo(map);
+var bounds = [[0,500], [1000, 1500]];
+var image = L.imageOverlay('image2.png', bounds).addTo(map);
 map.fitBounds(bounds);
 
 
 
-var marker = L.marker([538.125, 368.21875]).addTo(map);
+var marker = L.marker([749.96875, 745.390625]).addTo(map);
 marker.bindTooltip("City of Uruk", {permanent: true, className: "my-label", offset: [0, 0] });
 marker.addTo(map);
 
+var marker2 = L.marker([807.5625, 836.6875]).addTo(map);
+marker2.bindTooltip("Bargot River", {permanent: true, className: "my-label", offset: [0, 0] });
+marker2.addTo(map);
+
+var marker3 = L.marker([747.75, 914.25]).addTo(map);
+marker3.bindTooltip("Chentary Lake", {permanent: true, className: "my-label", offset: [0, 0] });
+marker3.addTo(map);
+
 var polygon = L.polygon([
-    [525.5, 345.1875],
-    [526.1875, 385.375],
-    [563.5, 384.859375],
-    [563.140625, 345.242188]
+    [743.09375, 729.703125],
+    [743.21875, 757.3125],
+    [764.225586, 756.905273],
+    [764.09375, 729.708984]
 ]).addTo(map);
 
 var popup = L.popup();
